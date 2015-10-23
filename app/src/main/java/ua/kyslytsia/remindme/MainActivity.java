5c6ccdfe0a2ed4dca2e3b7b87ae1eadf31e7f63f
keace <keace@rambler.ru>
2015-10-23 14:56:37 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/ua/kyslytsia/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package ua.kyslytsia.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Администратор on 23.10.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
